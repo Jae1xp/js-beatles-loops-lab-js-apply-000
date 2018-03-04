13fce308241532956d974fe75e3e7e79ebaad285
Jae Son <jae1xp@gmail.com>
2018-03-04 05:43:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function johnLennonFacts() {
   var newFacts = [];
   var i = 0;
   while (i < facts.length) {
-    facts.push(newFacts[i] + "!!!");
+    newFacts.push(facts[i] + "!!!");
     i++;
   }
   return newFacts;
